Project-Kaleidoscope/android_packages_apps_Camera

Camera: Add shortcuts
Co-authored-by: Shelling <cjybyjk@zjnu.edu.cn>
Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>
Change-Id: Ia140774a134aacedb4e0f18544d4a302559c8323

app/src/main/java/ink/kscope/camera/ui/activities/AutoActivity.java
@@ -1,0 +1,18 @@
+package ink.kscope.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import ink.kscope.camera.util.CameraModeUtil;
+
+public class AutoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.AUTO);
+        finish();
+    }
+}

app/src/main/java/ink/kscope/camera/ui/activities/CameraActivity.java
@@ -1,0 +1,18 @@
+package ink.kscope.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import ink.kscope.camera.util.CameraModeUtil;
+
+public class CameraActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.CAMERA);
+        finish();
+    }
+}

app/src/main/java/ink/kscope/camera/ui/activities/FaceRetouchActivity.java
@@ -1,0 +1,18 @@
+package ink.kscope.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import ink.kscope.camera.util.CameraModeUtil;
+
+public class FaceRetouchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.FACE_RETOUCH);
+        finish();
+    }
+}

app/src/main/java/ink/kscope/camera/ui/activities/NightActivity.java
@@ -1,0 +1,18 @@
+package ink.kscope.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import ink.kscope.camera.util.CameraModeUtil;
+
+public class NightActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.NIGHT);
+        finish();
+    }
+}

app/src/main/java/ink/kscope/camera/ui/activities/PortraitActivity.java
@@ -1,0 +1,18 @@
+package ink.kscope.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import ink.kscope.camera.util.CameraModeUtil;
+
+public class PortraitActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.PORTRAIT);
+        finish();
+    }
+}

app/src/main/java/ink/kscope/camera/ui/activities/ScannerActivity.java
@@ -1,0 +1,18 @@
+package ink.kscope.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import ink.kscope.camera.util.CameraModeUtil;
+
+public class ScannerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.QR_SCAN);
+        finish();
+    }
+}

app/src/main/java/ink/kscope/camera/ui/activities/VideoActivity.java
@@ -1,0 +1,18 @@
+package ink.kscope.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import ink.kscope.camera.util.CameraModeUtil;
+
+public class VideoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.VIDEO);
+        finish();
+    }
+}

app/src/main/java/ink/kscope/camera/util/CameraModeUtil.java
@@ -1,0 +1,16 @@
+package ink.kscope.camera.util;
+
+import android.content.Context;
+import android.content.Intent;
+
+import app.grapheneos.camera.CameraMode;
+import app.grapheneos.camera.ui.activities.MainActivity;
+
+public class CameraModeUtil {
+    public static void launchCameraWithMode(Context context, CameraMode mode) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra("mode", mode);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+}

LMODroid/platform_packages_apps_Camera

app/src/main/java/app/grapheneos/camera/ui/activities/AutoActivity.java
@@ -1,0 +1,18 @@
+package app.grapheneos.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import app.grapheneos.camera.util.CameraModeUtil;
+
+public class AutoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.AUTO);
+        finish();
+    }
+}

app/src/main/java/app/grapheneos/camera/ui/activities/CameraActivity.java
@@ -1,0 +1,18 @@
+package app.grapheneos.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import app.grapheneos.camera.util.CameraModeUtil;
+
+public class CameraActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.CAMERA);
+        finish();
+    }
+}

app/src/main/java/app/grapheneos/camera/ui/activities/FaceRetouchActivity.java
@@ -1,0 +1,18 @@
+package app.grapheneos.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import app.grapheneos.camera.util.CameraModeUtil;
+
+public class FaceRetouchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.FACE_RETOUCH);
+        finish();
+    }
+}

app/src/main/java/app/grapheneos/camera/ui/activities/NightActivity.java
@@ -1,0 +1,18 @@
+package app.grapheneos.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import app.grapheneos.camera.util.CameraModeUtil;
+
+public class NightActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.NIGHT);
+        finish();
+    }
+}

app/src/main/java/app/grapheneos/camera/ui/activities/PortraitActivity.java
@@ -1,0 +1,18 @@
+package app.grapheneos.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import app.grapheneos.camera.util.CameraModeUtil;
+
+public class PortraitActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.PORTRAIT);
+        finish();
+    }
+}

app/src/main/java/app/grapheneos/camera/ui/activities/ScannerActivity.java
@@ -1,0 +1,18 @@
+package app.grapheneos.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import app.grapheneos.camera.util.CameraModeUtil;
+
+public class ScannerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.QR_SCAN);
+        finish();
+    }
+}

app/src/main/java/app/grapheneos/camera/ui/activities/VideoActivity.java
@@ -1,0 +1,18 @@
+package app.grapheneos.camera.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import app.grapheneos.camera.CameraMode;
+import app.grapheneos.camera.util.CameraModeUtil;
+
+public class VideoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CameraModeUtil.launchCameraWithMode(getApplicationContext(), CameraMode.VIDEO);
+        finish();
+    }
+}

app/src/main/java/app/grapheneos/camera/util/CameraModeUtil.java
@@ -1,0 +1,16 @@
+package app.grapheneos.camera.util;
+
+import android.content.Context;
+import android.content.Intent;
+
+import app.grapheneos.camera.CameraMode;
+import app.grapheneos.camera.ui.activities.MainActivity;
+
+public class CameraModeUtil {
+    public static void launchCameraWithMode(Context context, CameraMode mode) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra("mode", mode);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+}